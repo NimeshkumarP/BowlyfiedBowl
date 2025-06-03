29b7f57708632d56dfeab85944b20f1c85117d2a
Lotachi Obi-Nwaigwe <W0873002@myscc.ca>
2025-06-03 12:21:45 -0400

    Lotachi-Leo

--- BowlyfiedBowl/Models/Bowl.cs
+++ BowlyfiedBowl/Models/Bowl.cs
@@ -7,20 +7,20 @@ namespace BolyfiedApp.Models
         public int Id { get; set; }
 
         [Required]
-        public string Material { get; set; }
+        public string? Material { get; set; }
 
-        public string Capacity { get; set; }
+        public string? Capacity { get; set; }
 
-        public string Dimensions { get; set; }
+        public string? Dimensions { get; set; }
 
-        public string Weight { get; set; }
+        public string? Weight { get; set; }
 
-        public string ColorOptions { get; set; }
+        public string? ColorOptions { get; set; }
 
         public bool MicrowaveSafe { get; set; }
 
         public bool DishwasherSafe { get; set; }
 
-        public string MadeIn { get; set; }
+        public string? MadeIn { get; set; }
     }
 }
